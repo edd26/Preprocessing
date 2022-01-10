8ab5b546ab2e4545feb76ff57ecae02af3c31a8e
edd26 <e.dmitruk@herts.ac.uk>
2022-01-10 10:45:18 +0000

    Added comments to the code

--- rsfMRI/C_ExtractTCAfterICA.sh
+++ rsfMRI/C_ExtractTCAfterICA.sh
@@ -4,11 +4,11 @@ SUBDIR=../../B_Data/C_FSLPreproc
 FSLDIR=../../../Z_Tools/fsl
 MSKDIR=../../../Z_Tools/AALMasks
 function rp {
-    
+
     if [ ! -d $1/rsfMRI/B_ExtractedTCs ]; then
         mkdir $1/rsfMRI/B_ExtractedTCs
         mkdir $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace
-    fi    
+    fi
     dirname=$2
     result=${dirname%%+(/)}    # trim however many trailing slashes exist
     result=${result##*/}
@@ -16,9 +16,16 @@ function rp {
 
     if [ ! -f $1/rsfMRI/B_ExtractedTCs/$result.txt ]; then
         echo "Processing subject: $1 . area $2"
+        # Move processed data into MNI space
         flirt -in $2 -ref $1/rsfMRI/A_PreprocOutput.feat/example_func.nii.gz -init $1/rsfMRI/A_PreprocOutput.feat/reg/standard2example_func.mat -applyxfm -out $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace/$result\_func.nii.gz
-        fslmaths $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace/$result\_func.nii.gz -thr 0.5 -bin $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace/$result\_func.nii.gz 
+
+        # Do the masking?
+        fslmaths $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace/$result\_func.nii.gz -thr 0.5 -bin $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace/$result\_func.nii.gz
+        fslmaths $INPUT_FILE -thr 0.5 -bin
+
+        # get the time course?
         fslmeants -i $1/rsfMRI/A_PreprocOutput.feat/filtered_func_data_clean.nii.gz -o $1/rsfMRI/B_ExtractedTCs/$result.txt -m $1/rsfMRI/B_ExtractedTCs/A_AAL2SubjectSpace/$result\_func.nii.gz
+        fslmeants -i $FILTERED_DATA_INPUT_FILE -o $OUTPUT_FILE -m $MASK_FILE
         echo "Finished processing of subject $1 , area $2. "
     fi
 }
@@ -34,7 +41,7 @@ N=2
 for subjpath in $SUBDIR/*
 do
     for maskpath in $MSKDIR/*
-    do        
+    do
     	rp $subjpath $maskpath &
         pwait 2
     done
