528033238ae5aecf12577db1b3c12619668d6a0c
ed18abc <ed18abc@herts.ac.uk>
2022-03-16 14:02:49 +0000

    Fixed folders names; changed iteration sequencing

--- voxel_export/copy_voxel_exports.sh
+++ voxel_export/copy_voxel_exports.sh
@@ -2,19 +2,21 @@
 
 set -e
 
+BRAIN_REGION=$1
+
 # Subjects
-for i in `seq -f "%01g" 1 1`; do
+for i in `seq -f "%02g" 1 1`; do
 
     # Session
-    for f in `seq -f "%01g" 1 1`; do
+    for f in `seq -f "%02g" 1 3`; do
 
         # Run
-        for r in `seq -f "%01g" 1 2`; do
+        for r in `seq -f "%02g" 1 2`; do
 
             # Side
             for s in "R" "L"; do
-                SRC_FOLDER="00${i}_sub-MSC0${i}_ses-func0${f}_task-motor_run-0${r}_bold_brain.feat"
-                SUBFOLDER="MSC0${i}_ses${f}_motor_run0${r}_Supp_Motor_Area_${s}_masked_voxel_export"
+                SRC_FOLDER="./0${i}/0${i}_sub-MSC${i}_ses-func${f}_task-motor_run-${r}_bold_brain.feat"
+                SUBFOLDER="MSC${i}_ses${f}_motor_run${r}_${BRAIN_REGION}_${s}_voxel_export"
                 TARGET_DIR="${HOME}/Programming/Julia/SchiTopology/data/exp_raw/voxel_data/whole_brain/"
 
 
