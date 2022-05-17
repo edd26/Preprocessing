91d8797b707bade83d2ef4f5172b69adda95e7b8
ed18abc <ed18abc@herts.ac.uk>
2022-05-17 16:43:30 +0100

    Extended echo print; fixed script path

--- MSC_exporting/3_export_single_AAL2_region_for_MSC.sh
+++ MSC_exporting/3_export_single_AAL2_region_for_MSC.sh
@@ -22,7 +22,7 @@ BRAIN_REGION=$2
 
 
 # Subjects
-for i in `seq -f "%02g" 1 3`; do
+for i in `seq -f "%02g" 1 1`; do
 
     # Session
     for f in `seq -f "%02g" 1 3`; do
@@ -33,14 +33,14 @@ for i in `seq -f "%02g" 1 3`; do
             WORKING_DIRECTORY="./0${i}/ses-func${f}/func/sub-MSC${i}_ses-func${f}_task-motor_run-${r}_bold_brain.feat"
             SESSION_NAME="MSC${i}_ses${f}_motor_run${r}"
 
-            echo "Working in: " $WORKING_DIRECTORY
+            echo "Working in: " $WORKING_DIRECTORY $SESSION_NAME
 
             # Side
             for s in "L" "R"; do
                 FINAL_BRAIN_REGION="${BRAIN_REGION}_${s}"
                 echo "With region: " $FINAL_BRAIN_REGION
-
-                ./../voxel_export_templates/symmetric_voxels_signal_export.sh ${WORKING_DIRECTORY} ${SESSION_NAME} ${MASKS_PATH} ${FINAL_BRAIN_REGION}
+                
+                ./../brain-scripting/voxel_export_templates/symmetric_voxels_signal_export.sh ${WORKING_DIRECTORY} ${SESSION_NAME} ${MASKS_PATH} ${FINAL_BRAIN_REGION}
             done # s
         done # r
     done # f
