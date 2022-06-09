511d014c34cb0f4b4008fcd99f345e21767b6cce
ed18abc <ed18abc@herts.ac.uk>
2022-06-09 14:05:04 +0100

    Refactored structure; unified input arguments

--- MSC_exporting/1_run_BET_for_MSC.sh
+++ MSC_exporting/1_run_BET_for_MSC.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # DESCRIPTION:
 # Runs brain estraction tool for the selected file under DATA_PATH.
 # Script written for MSC data which hadn multiple sessions.
@@ -12,60 +10,58 @@ set -e
 
 # TODO:
 # - add parameter to have 4D data processed, not only a single frame (default)
+# ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
+set -e
 
+source naming_functions
 
 # ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
 # Handle input arguments
 
-SUBJECT_ID=$1
+# SUBJECT_ID=$1
+SUBEJCTS_MIN=$1
+SUBEJCTS_MAX=$2
+TOTAL_SESSIONS=$3
+TOTAL_RUNS=$4
 
-TOTAL_SESSIONS=$2
-
-F_VAL=$3
+F_VAL=$5
 
 # ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
 # CONSTANTS
 BET_PATH="/usr/local/fsl/bin/bet"
+EXTENSION=".nii"
 
 # ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
 # Run the analysis
-
-EXTENSION=".nii"
 
 echo
 echo "===-===-===-"
 echo "Running BET"
 echo
-for i in `seq -f "%02g" $SUBJECT_ID $SUBJECT_ID `; do
+# Subjects
+for i in `seq -f "%02g" $SUBEJCTS_MIN $SUBEJCTS_MAX`; do
+    
+    # Sessions
     for f in `seq -f "%02g" 1 $TOTAL_SESSIONS`; do
+        
         DATA_PATH="0${i}"/"ses-func${f}"/"func"
-
-        for TASK in "glasslexical_run-01" "glasslexical_run-02" "memoryfaces" "memoryscenes" "memorywords" "motor_run-01" "motor_run-02"; do
-
-            IN_FILE=$DATA_PATH/"sub-MSC${i}_ses-func${f}_task-${TASK}_bold"
-            FULL_IN_FILE=$IN_FILE$EXTENSION
-            OUT_FILE="${IN_FILE}_brain.nii.gz"
-
-            if [[ -e $FULL_IN_FILE ]]; then
-                echo "Input file exists!"
+        
+        for TASK in "glasslexical" "memoryfaces" "memoryscenes" "memorywords" "motor"; do
+            
+            if [[ "${TASK}" == "motor" ]] || [[ "${TASK}" == "glasslexical" ]]; then
+                # Runs
+                for r in `seq -f "%02g" 1 2`; do
+                    get_BET_done
+                done # r
             else
-                echo "Input file does not exists!"
-            fi
-            echo $FULL_IN_FILE
+                r=1
+                get_BET_done
+            fi # tasks
+            
+        done # TASK
+    done # f
+done # i
 
-            if [[ -e $OUT_FILE ]]; then
-                echo $OUT_FILE
-                echo " file exists. Skipping..."
-            else
-                echo "Running BET"
-                echo $BET_PATH $IN_FILE $OUT_FILE -F -f $F_VAL -g 0
-
-                $BET_PATH $IN_FILE $OUT_FILE -F -f $F_VAL -g 0
-            fi
-            echo
-        done # f
-    done # i
-done
 echo "===-===-===-===-"
 
 # ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
